45ea064dc783a0fb0f28b7de9611a7766f1a888a
zhouand000 <zhouand000@users.noreply.github.com>
2015-05-16 19:37:29 -0500

    Various Changes

---
+++ src/arpg/game/Player.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package arpg.game;
+
+/**
+ * @author Andrew
+ * 
+ */
+public class Player {
+	
+	/**
+	 * 
+	 */
+	public Player () {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+}
---
+++ src/arpg/game/events/EventRandomizer.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package arpg.game.events;
+
+/**
+<<<<<<< HEAD
+=======
+ * Basic events prototype
+ * 
+>>>>>>> 7bb3b6dc2b7586b9da392da3af1631d6e36d480e
+ * @author Andrew
+ * 
+ */
+public class EventRandomizer {
+	
+
+	/**
+	 * 
+	 */
+	public EventRandomizer () {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
